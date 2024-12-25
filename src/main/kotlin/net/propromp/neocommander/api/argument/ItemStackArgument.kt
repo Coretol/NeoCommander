c1f95ca8f033561ba0e36f24defbcee5d180a953
bea4dev <bea0224@outlook.jp>
2024-12-25 15:57:40 +0900

    fix 1.21.1

--- src/main/kotlin/net/propromp/neocommander/api/argument/ItemStackArgument.kt
+++ src/main/kotlin/net/propromp/neocommander/api/argument/ItemStackArgument.kt
@@ -2,15 +2,26 @@ package net.propromp.neocommander.api.argument
 
 import com.mojang.brigadier.arguments.ArgumentType
 import io.papermc.paper.command.brigadier.argument.ArgumentTypes
+import net.minecraft.commands.CommandBuildContext
 import net.minecraft.commands.arguments.item.ItemArgument
 import net.minecraft.commands.arguments.item.ItemInput
 import net.propromp.neocommander.api.NeoCommandContext
+import org.bukkit.Bukkit
+import org.bukkit.craftbukkit.CraftServer
 import org.bukkit.craftbukkit.inventory.CraftItemStack
 import org.bukkit.inventory.ItemStack
 
+private val COMMAND_BUILDER_CONTEXT = run {
+    val server = (Bukkit.getServer() as CraftServer).handle
+    CommandBuildContext.simple(
+        server.server.registryAccess(),
+        server.server.worldData.dataConfiguration.enabledFeatures()
+    )
+}
+
 @Suppress("UnstableApiUsage")
 class ItemStackArgument(name: String) : NeoArgument<Any, ItemStack>(name) {
-    override fun asBrigadier(): ArgumentType<ItemStack> = ArgumentTypes.itemStack()
+    override fun asBrigadier(): ArgumentType<*> = ItemArgument.item(COMMAND_BUILDER_CONTEXT)
 
     override fun parse(context: NeoCommandContext, t: Any): ItemStack {
         val itemStack = (t as ItemInput).createItemStack(1, false)
