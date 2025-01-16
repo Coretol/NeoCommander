6814543c8db7d10a6d11e16d1bf710b8f81018b7
bea4dev <bea0224@outlook.jp>
2025-01-16 22:03:46 +0900

    fix 1.21.1

--- src/main/kotlin/net/propromp/neocommander/api/argument/EntityListArgument.kt
+++ src/main/kotlin/net/propromp/neocommander/api/argument/EntityListArgument.kt
@@ -1,6 +1,21 @@
 package net.propromp.neocommander.api.argument
 
+import com.mojang.brigadier.arguments.ArgumentType
+import net.minecraft.commands.CommandSourceStack
+import net.minecraft.commands.arguments.EntityArgument
+import net.minecraft.commands.arguments.selector.EntitySelector
+import net.propromp.neocommander.api.NeoCommandContext
 import org.bukkit.entity.Entity
 
 class EntityListArgument(name: String) :
-    AbstractEntityArgument<List<Entity>>(name, SEVERAL_ENTITIES)
+    AbstractEntityArgument<List<Entity>>(name) {
+
+    override fun asBrigadier(): ArgumentType<out Any> {
+        return EntityArgument.entities()
+    }
+
+    override fun parse(context: NeoCommandContext, t: Any): List<Entity> {
+        return (t as EntitySelector).findEntities(context.context.source as CommandSourceStack)
+            .map { entity -> entity.bukkitEntity }
+    }
+}
